9a2da55b1ba4024ee6be54f66b12e3673fbf032c
Lucas Guimaraes Moreira <103656598+JkaiPrime@users.noreply.github.com>
2023-11-29 21:42:10 -0300

    Finalizado a activity de envio da lista de Candidatos por outras aplicações pelo Renato

--- app/src/main/java/com/example/gigroupacompanhamentos/View/EnviarListaEmail.kt
+++ app/src/main/java/com/example/gigroupacompanhamentos/View/EnviarListaEmail.kt
@@ -6,7 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.Menu
-import android.widget.Toast
 import com.example.gigroupacompanhamentos.R
 import com.example.gigroupacompanhamentos.ROOM.Pessoa
 import com.example.gigroupacompanhamentos.ROOM.PessoaDAO
@@ -14,50 +13,55 @@ import com.example.gigroupacompanhamentos.ROOM.PessoaDB
 import com.example.gigroupacompanhamentos.databinding.ActivityEnviarListaEmailBinding
 import java.lang.Exception
 
-class EnviarListaEmail : MainActivity() {
+class EnviarListaEmail : AppCompatActivity() {
+
     private lateinit var binding: ActivityEnviarListaEmailBinding
     private lateinit var dao: PessoaDAO
-    private fun SendList(){
+
+    private fun sendList() {
         val message = formatarLista(dao.getAll())
-        val target = binding.Target.toString()
-        val recipiente = binding.Recipiente.toString()
-        val mailItente = Intent(Intent.ACTION_SEND)
-        mailItente.data = Uri.parse("MailTo")
-        mailItente.type = "Processo Seletivo"
-        mailItente.putExtra(Intent.EXTRA_EMAIL, arrayOf(recipiente))
-        mailItente.putExtra(Intent.EXTRA_SUBJECT, target)
-        mailItente.putExtra(Intent.EXTRA_TEXT, message)
+        val target = binding.Target.text.toString()
+        val recipiente = binding.Recipiente.text.toString()
+        val mailIntent = Intent(Intent.ACTION_SEND)
+        mailIntent.data = Uri.parse("mailto:")
+        mailIntent.type = "text/plain"
+        mailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(recipiente))
+        mailIntent.putExtra(Intent.EXTRA_SUBJECT, target)
+        mailIntent.putExtra(Intent.EXTRA_TEXT, message)
+
         try {
-            startActivity(Intent.createChooser(mailItente, "Send E-mail to:${target}"))
-        }catch (e: Exception){
+            startActivity(Intent.createChooser(mailIntent, "Send E-mail to: $target"))
+        } catch (e: Exception) {
             Log.d("Erro na execução do envio", e.toString())
         }
     }
-    private fun formatarLista(lista: List<Pessoa>):String{
+
+    private fun formatarLista(lista: List<Pessoa>): String {
         val mensagemFormatada = StringBuilder()
         try {
-            for (item in lista){
+            for (item in lista) {
                 mensagemFormatada.append(item).append("\n")
             }
             return mensagemFormatada.toString()
-        }catch (e: Exception){
+        } catch (e: Exception) {
             Log.d("Erro na formatação da Lista", e.toString())
         }
         return mensagemFormatada.toString()
     }
+
     override fun onPrepareOptionsMenu(menu: Menu): Boolean {
         val menuItemToHide = menu.findItem(R.id.enviar)
         menuItemToHide?.isVisible = false
-
         return super.onPrepareOptionsMenu(menu)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEnviarListaEmailBinding.inflate(layoutInflater)
         setContentView(binding.root)
         dao = PessoaDB.getInstance(this).getDao()
         binding.btnEnviar.setOnClickListener {
-            SendList()
+            sendList()
         }
     }
 }
